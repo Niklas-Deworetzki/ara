0a00e141f2736aac73a9c57473e9d4e1c988df6d
Niklas Deworetzki <47612126+Niklas-Deworetzki@users.noreply.github.com>
2023-05-31 19:56:01 +0200

    Actually implemented ResourcePath class.

--- src/main/kotlin/ara/storage/ResourcePath.kt
+++ src/main/kotlin/ara/storage/ResourcePath.kt
@@ -2,20 +2,26 @@ package ara.storage
 
 import ara.syntax.Syntax
 
-interface ResourcePath {
+class ResourcePath
+private constructor(private val path: List<String>) {
 
     val indices: Iterable<Int>
-        get() = TODO()
+        get() = path.indices
 
-    operator fun get(index: Int): String = TODO()
+    operator fun get(index: Int): String =
+        path[index]
 
-    fun subPath(indexIncluded: Int): ResourcePath = TODO()
+    fun subPath(indexIncluded: Int): ResourcePath =
+        ResourcePath(path.subList(0, indexIncluded))
 
-    fun appended(accessor: String): ResourcePath = TODO()
+    fun appended(accessor: Syntax.Identifier): ResourcePath =
+        ResourcePath(path + accessor.name)
 
     companion object {
-        fun of(segments: Iterable<String>): ResourcePath = TODO()
+        fun of(segments: Iterable<String>): ResourcePath =
+            ResourcePath(segments.toList())
 
-        fun ofIdentifier(name: Syntax.Identifier): ResourcePath = TODO()
+        fun ofIdentifier(name: Syntax.Identifier): ResourcePath =
+            ResourcePath(listOf(name.name))
     }
 }

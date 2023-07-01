6810fa7b78a50e76d31fcffee8d846b7b3750f5c
Niklas Deworetzki <47612126+Niklas-Deworetzki@users.noreply.github.com>
2023-07-01 13:53:46 +0200

    Removed wrapper class from utility functions.

--- src/main/kotlin/ara/utils/Collections.kt
+++ src/main/kotlin/ara/utils/Collections.kt
@@ -1,32 +1,29 @@
 package ara.utils
 
-object Collections {
+fun <X, Y> combineWith(xs: Iterable<X>, ys: Iterable<Y>, action: (X, Y) -> Unit) {
+    val xsIterator = xs.iterator()
+    val ysIterator = ys.iterator()
+    while (xsIterator.hasNext() && ysIterator.hasNext()) {
+        action(xsIterator.next(), ysIterator.next())
+    }
+}
 
-    fun <X, Y> combineWith(xs: Iterable<X>, ys: Iterable<Y>, action: (X, Y) -> Unit) {
-        val xsIterator = xs.iterator()
-        val ysIterator = ys.iterator()
-        while (xsIterator.hasNext() && ysIterator.hasNext()) {
-            action(xsIterator.next(), ysIterator.next())
-        }
+fun <K, V> zipToMap(keys: Iterable<K>, values: Iterable<V>): MutableMap<K, V> {
+    val keyIterator = keys.iterator()
+    val valueIterator = values.iterator()
+    val resultMap = mutableMapOf<K, V>()
+
+    while (keyIterator.hasNext() && valueIterator.hasNext()) {
+        resultMap[keyIterator.next()] = valueIterator.next()
     }
 
-    fun <K, V> zipToMap(keys: Iterable<K>, values: Iterable<V>): MutableMap<K, V> {
-        val keyIterator = keys.iterator()
-        val valueIterator = values.iterator()
-        val resultMap = mutableMapOf<K, V>()
+    return resultMap
+}
 
-        while (keyIterator.hasNext() && valueIterator.hasNext()) {
-            resultMap[keyIterator.next()] = valueIterator.next()
-        }
+fun <T> sublist(list: List<T>, skipFront: Int, skipEnd: Int): List<T> {
+    return list.subList(skipFront, list.size - skipEnd)
+}
 
-        return resultMap
-    }
-
-    fun <T> sublist(list: List<T>, skipFront: Int, skipEnd: Int): List<T> {
-        return list.subList(skipFront, list.size - skipEnd)
-    }
-
-    fun <T> symmetricDifference(a: Set<T>, b: Set<T>): Set<T> {
-        return (a - b) + (b - a)
-    }
+fun <T> symmetricDifference(a: Set<T>, b: Set<T>): Set<T> {
+    return (a - b) + (b - a)
 }

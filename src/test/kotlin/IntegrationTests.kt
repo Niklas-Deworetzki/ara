2459f7e577e2c6f0894855812385f60fca9a646e
Niklas Deworetzki <47612126+Niklas-Deworetzki@users.noreply.github.com>
2023-07-02 10:24:25 +0200

    Updated test suite.

--- src/test/kotlin/IntegrationTests.kt
+++ src/test/kotlin/IntegrationTests.kt
@@ -1,4 +1,6 @@
 import ara.analysis.Analysis
+import ara.input.Scanner
+import ara.input.Token
 import ara.position.InputSource
 import ara.reporting.MessageFormatter
 import org.junit.jupiter.api.DynamicContainer
@@ -6,11 +8,9 @@ import org.junit.jupiter.api.DynamicNode
 import org.junit.jupiter.api.DynamicTest
 import org.junit.jupiter.api.TestFactory
 import java.io.File
-import java.nio.file.Files
-import kotlin.test.fail
 
-private const val SUCCESS_HEADER = "// Success"
-private const val ERROR_HEADER = "// Error: "
+private const val POSITIVE_TEST_HEADER = "Success: "
+private const val NEGATIVE_TEST_HEADER = "Error: "
 
 class IntegrationTests {
 
@@ -20,18 +20,21 @@ class IntegrationTests {
 
     private fun createTestsFromFile(file: File): DynamicNode = when {
         file.isDirectory -> {
-            val subTests = file.listFiles()!!.map { createTestsFromFile(it) }
+            val subTests = file.listFiles()!!
+                .sortedBy { it.name }
+                .map { createTestsFromFile(it) }
             DynamicContainer.dynamicContainer(file.name, subTests)
         }
 
-        else ->
-            DynamicTest.dynamicTest(file.name) {
-                verifyTestProgram(file)
+        else -> {
+            val specification = parseTestSpecification(file)
+            DynamicTest.dynamicTest(file.name + ": " + specification.description) {
+                verifyTestProgram(specification, file)
             }
+        }
     }
 
-    private fun verifyTestProgram(file: File) {
-        val expectedResult = parseExpectedResult(file)
+    private fun verifyTestProgram(specification: TestSpec, file: File) {
         val analysis = Analysis.ofInput(InputSource.fromFile(file))
 
         analysis.runAnalysis()
@@ -39,34 +42,51 @@ class IntegrationTests {
             println(MessageFormatter.format(error))
         }
 
-        when (expectedResult) {
-            Success ->
+        when (specification) {
+            is PositiveTest ->
                 assert(!analysis.hasReportedErrors) {
                     "Analysis should not have reported errors, but ${analysis.reportedErrors.size} were found!"
                 }
 
-            is Error ->
-                assert(analysis.reportedErrors.any { it.message.lines().first() == expectedResult.message }) {
-                    "Analysis should report at least one error with message: ${expectedResult.message}"
+            is NegativeTest -> {
+                assert(specification.errors.isNotEmpty()) {
+                    "Negative tests must specify at least 1 expected error."
                 }
+
+                for (error in specification.errors) {
+                    assert(analysis.reportedErrors.any { it.message == error }) {
+                        "Analysis should report at least one error with message: $error"
+                    }
+                }
+            }
         }
     }
 
-    private fun parseExpectedResult(file: File): ExpectedResult {
-        val lines = Files.readAllLines(file.toPath())
-        assert(lines.size >= 1) {
-            "Header line must be present in ${file.absolutePath}."
+    private fun parseTestSpecification(file: File): TestSpec =
+        Scanner(InputSource.fromFile(file)).use { scanner ->
+            val lines = mutableListOf<String>()
+
+            var token = scanner.nextToken()
+            while (token.type == Token.Type.COMMENT) {
+                val comment = token.value!!.trim()
+                lines.add(comment)
+                token = scanner.nextToken()
+            }
+
+            val header = lines.firstOrNull()
+            when {
+                header != null && header.startsWith(POSITIVE_TEST_HEADER) ->
+                    PositiveTest(header.substring(POSITIVE_TEST_HEADER.length).trim())
+
+                header != null && header.startsWith(NEGATIVE_TEST_HEADER) ->
+                    NegativeTest(header.substring(NEGATIVE_TEST_HEADER.length).trim(), lines.drop(1))
+
+                else ->
+                    PositiveTest(null)
+            }
         }
 
-        val header = lines.first()
-        return when {
-            header.startsWith(SUCCESS_HEADER) -> Success
-            header.startsWith(ERROR_HEADER) -> Error(header.drop(ERROR_HEADER.length))
-            else -> fail("Invalid header line: $header")
-        }
-    }
-
-    sealed class ExpectedResult
-    object Success : ExpectedResult()
-    data class Error(val message: String) : ExpectedResult()
+    sealed class TestSpec(val description: String)
+    class PositiveTest(description: String?) : TestSpec(description ?: "A regular program should pass analysis.")
+    class NegativeTest(description: String, val errors: List<String>) : TestSpec(description)
 }
